006703500dd4bcdd1279efe02eb6878ab6ad10f4
Zhurbey Artem <zhurbeyartem@gmail.com>
2024-01-06 22:08:31 +0200

    hm3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,15 @@ import s from 'index.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts:
+      JSON.parse(localStorage.getItem('contacts')).length > 0
+        ? JSON.parse(localStorage.getItem('contacts'))
+        : [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+          ],
     filter: '',
   };
 
@@ -29,15 +32,23 @@ export class App extends Component {
     }
 
     const newContact = { id: nanoid(), name, number: phone };
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+
+    this.setState(
+      prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }),
+      () => {
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      }
+    );
   };
 
   removeUser = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+    const updatedContacts = this.state.contacts.filter(
+      contact => contact.id !== id
+    );
+    this.setState({ contacts: updatedContacts });
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
   };
 
   setFilter = val => {
@@ -46,7 +57,6 @@ export class App extends Component {
 
   filteredContacts = () => {
     const { filter, contacts } = this.state;
-
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
